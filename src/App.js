535806d406a0027d15f539422c4a840b1434497c
rkaku <greenapple728@icloud.com>
2019-11-17 19:34:39 +0900

    nav link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, NavLink, useLocation, Switch } from 'react-router-dom'
 
-function App() {
+function App () {
+  const location = useLocation()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {
+              location.pathname !== '/' && (
+                <li>
+                  <NavLink
+                    className="App-link"
+                    to="/"
+                    exact
+                    activeClassName="active-link"
+                  >
+                  </NavLink>
+                </li>
+              )
+            }
+            {
+              location.pathname !== '/wallet' && (
+                <li>
+                  <NavLink
+                    className="App-link"
+                    to="/wallet"
+                    exact
+                    activeClassName="active-link"
+                  >
+                  </NavLink>
+                </li>
+              )
+            }
+            {
+              location.pathname !== '/pool' && (
+                <li>
+                  <NavLink
+                    className="App-link"
+                    to="/pool"
+                    exact
+                    activeClassName="active-link"
+                  >
+                  </NavLink>
+                </li>
+              )
+            }
+            {
+              location.pathname !== '/chain' && (
+                <li>
+                  <NavLink
+                    className="App-link"
+                    to="/chain"
+                    exact
+                    activeClassName="active-link"
+                  >
+                  </NavLink>
+                </li>
+              )
+            }
+          </ul>
+          <Switch>
+            <Route path="/wallet" />
+            <Route path="/pool" />
+            <Route path="/chain" />
+            <Route path="/" component={ App } />
+          </Switch>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
