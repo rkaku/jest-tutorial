5e953f06958f86f7d6ed29c624552456880124cb
rkaku <greenapple728@icloud.com>
2019-11-24 08:56:22 +0900

    axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
+
 import { getLetterMatchCount } from './../helpers';
 
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 
@@ -27,7 +30,19 @@ export const guessWord = ( guessedWord ) => {
     } );
 
     if ( guessedWord === secretWord ) {
-      dispatch({type: actionTypes.CORRECT_GUESS});
+      dispatch( { type: actionTypes.CORRECT_GUESS } );
     }
+  };
+};
+
+export const getSecretWord = () => {
+  return ( dispatch ) => {
+    return axios.get( 'http://localhost:3030' )
+      .then( response => {
+        dispatch( {
+          type: actionTypes.SET_SECRET_WORD,
+          payload: response.data
+        } );
+      } )
   };
 };
